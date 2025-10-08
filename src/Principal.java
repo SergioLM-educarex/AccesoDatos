9d2885bae7d9e2b5d02b5f71462c27e07b6da230
dam23 <dam23@10.0.2.15>
2025-10-08 12:56:51 +0200

    inicio

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		
+		sergio();
+		
+	}
+
+	private static void sergio() {
+		
+		
+	}
+
+}
