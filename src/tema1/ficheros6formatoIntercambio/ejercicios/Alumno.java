e3fcd92eaffc4db3acfbf3dfd89655f3fb5ed5d1
SergioLeonMateos <sergio.leon.mateos@gmail.com>
2025-10-25 10:04:49 +0200

    Ejercio37

--- src/tema1/ficheros6formatoIntercambio/ejercicios/Alumno.java
+++ src/tema1/ficheros6formatoIntercambio/ejercicios/Alumno.java
@@ -1,6 +1,6 @@
 package tema1.ficheros6formatoIntercambio.ejercicios;
 
-public class Alumno {
+public class Alumno implements Comparable<Alumno> {
 
 	private int numExpediente;
 	private String nombre;
@@ -13,30 +13,50 @@ public class Alumno {
 		this.nombre = nombre;
 		this.nota = nota;
 	}
+
+
 	public int getNumExpediente() {
 		return numExpediente;
 	}
+
+
 	public void setNumExpediente(int numExpediente) {
 		this.numExpediente = numExpediente;
 	}
+
+
 	public String getNombre() {
 		return nombre;
 	}
+
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+
 	public double getNota() {
 		return nota;
 	}
+
+
 	public void setNota(double nota) {
 		this.nota = nota;
 	}
+
+
 	@Override
 	public String toString() {
 		return "Alumno [numExpediente=" + numExpediente + ", nombre=" + nombre + ", nota=" + nota + "]";
 	}
+
+
+	@Override
+	public int compareTo(Alumno o) {
+		
+		return Integer.compare(numExpediente, o.numExpediente);
+	}
+
 	
-	
-	
-	
+
 }
--- src/tema1/ficheros6formatoIntercambio/ejercicios/Ejercicio37.java
+++ src/tema1/ficheros6formatoIntercambio/ejercicios/Ejercicio37.java
@@ -1,5 +1,6 @@
 package tema1.ficheros6formatoIntercambio.ejercicios;
 
+import java.io.File;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -7,87 +8,144 @@ import java.util.TreeSet;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 
 public class Ejercicio37 {
 
 	private static final int NO = 2;
 	private static final int SI = 1;
 	private static Scanner entrada = new Scanner(System.in);
+	private static Set<Alumno> conjuntoAlumnos = new TreeSet<Alumno>();
 
 	public static void main(String[] args) {
 
+		// ------------------------------
+		// 1. Bucle de entrada de datos
+		// ------------------------------
 		boolean continuar = true;
-		String nombre = "";
-		double nota;
-		int numExpe, num = 1;
-		Set<Alumno> conjuntoAlumnos = new TreeSet<Alumno>();
+		int num = 1;
 
 		while (num == SI) {
-
-			System.out.println("----- PROGRAMA PARA A�ADIR NOTAS EXPEDIENTE ----");
+			System.out.println("----- PROGRAMA PARA AñADIR NOTAS EXPEDIENTE ----");
 			try {
+				// Pedimos número de expediente
 				System.out.println("Inserte el numero de Expediente");
-				numExpe = Integer.parseInt(entrada.nextLine());
+				int numExpe = Integer.parseInt(entrada.nextLine());
 
+				// Pedimos nombre del alumno
 				System.out.println("Inserte el nombre");
-				nombre = entrada.nextLine();
+				String nombre = entrada.nextLine();
 
+				// Pedimos la nota
 				System.out.println("Inserte la nota");
-				nota = Double.parseDouble(entrada.nextLine());
+				double nota = Double.parseDouble(entrada.nextLine());
 
-				System.out.println("�Quieres continuar? 1-SI - 2.NO");
+				// Preguntamos si quiere continuar
+				System.out.println("¿Quieres continuar? 1-SI / 2-NO");
 				num = Integer.parseInt(entrada.nextLine());
 
+				// Crear objeto Alumno y añadir al TreeSet
 				Alumno alumno = new Alumno(numExpe, nombre, nota);
-
 				conjuntoAlumnos.add(alumno);
 
 			} catch (NumberFormatException e) {
-				System.out.println("Error!! Introduce un numero");
+				System.out.println("Error!! Introduce un numero válido");
 			}
+		} // fin del while
 
-			// Creamos la factor�a
-			DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
-			// Creamos el DocumentBuilder
-			DocumentBuilder db;
-			try {
-				db = factoria.newDocumentBuilder();
-				// Creamos un DOM vac�o
-				Document documento = db.newDocument();
-				// Establecemos la versi�n
-				documento.setXmlVersion("1.0");
-				
-				
-				// Creamos elementos y a�adimos al DOM
-				Element elementoRaiz = documento.createElement("Alumnos");	
-				
-				
-				
-				for (Alumno alumno : conjuntoAlumnos) {
-				
-				Element El_alumno = documento.createElement("Alumno");
-				elementoRaiz.appendChild(El_alumno);
-				
-				Element num_Expediente = documento.createElement();
-				
-				}
-				
-				
-				
-				
-			} catch (ParserConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-			
-			
+		// ------------------------------
+		// 2. Crear estructura del DOM
+		// ------------------------------
+		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = null;
+		try {
+			db = factoria.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
 		}
 
-	}
+		// Creamos un DOM vacío
+		Document documento = db.newDocument();
+		// Establecemos la versión XML
+		documento.setXmlVersion("1.0");
 
+		// Creamos la raíz y añadimos al DOM
+		Element elementoRaiz = documento.createElement("Alumnos");
+
+		// ------------------------------
+		// 3. Recorrer TreeSet y crear elementos XML
+		// ------------------------------
+		for (Alumno alumno : conjuntoAlumnos) {
+
+			// Creo el elemento Alumno para añadirle toda la información
+			Element el_alumno = documento.createElement("Alumno");
+
+			// Creo el Nodo num_expediente con el fin de añadirle un nodo de tipo Texto
+			// Este nodo tipo texto se hace desde el Document, no se crea otro Element
+			Element num_Expediente = documento.createElement("Expediente");
+			Text t_numexp = documento.createTextNode(String.valueOf(alumno.getNumExpediente()));
+			num_Expediente.appendChild(t_numexp);
+			el_alumno.appendChild(num_Expediente);
+
+			// ----------------
+			// Nombre
+			// Este paso es directo, añade desde el append child,
+			// sin necesidad de crear un objeto Text
+			Element nombreAlumno = documento.createElement("Nombre");
+			nombreAlumno.appendChild(documento.createTextNode(alumno.getNombre()));
+			el_alumno.appendChild(nombreAlumno);
+
+			// ------------------
+			// Nota
+			Element notaAlumno = documento.createElement("Nota");
+			notaAlumno.appendChild(documento.createTextNode(String.valueOf(alumno.getNota())));
+			el_alumno.appendChild(notaAlumno);
+
+			// Añadir <Alumno> a la raíz
+			elementoRaiz.appendChild(el_alumno);
+		}
+
+		// ------------------------------
+		// 4. Añadir raíz al documento
+		// ------------------------------
+		documento.appendChild(elementoRaiz);
+
+		// ------------------------------
+		// 5. Transformar y guardar XML
+		// ------------------------------
+		try {
+			// 1. Crear una fuente/origen con el árbol DOM
+			DOMSource fuente = new DOMSource(documento);
+			// 2. Crear el destino de la transformación (archivo)
+			StreamResult ficheroXML = new StreamResult(new File("notasALumnado.xml"));
+			// 2.1. Si queremos mostrar por consola
+			StreamResult consola = new StreamResult(System.out);
+
+			// 3. Crear un transformador
+			Transformer t = TransformerFactory.newInstance().newTransformer();
+			// 4. Mejoramos el formato de salida
+			t.setOutputProperty(OutputKeys.INDENT, "yes");
+			// 5. Transformar la fuente en el resultado
+			t.transform(fuente, ficheroXML);
+			// 5.1. Mostrar por consola
+			t.transform(fuente, consola);
+
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+
+		// ------------------------------
+		// 6. Cerrar Scanner
+		// ------------------------------
+		entrada.close();
+	}
 }
