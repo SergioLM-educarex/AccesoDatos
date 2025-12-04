f409fbfa147d0318f0fe03260c58023cbd8342c7
SergioLM-educarex <sleonm03@educarex.es>
2025-12-04 10:21:00 +0100

    Trabajo 04/12

--- src/tema2ManejoConectores/ejercicios10/NavalvintedAPP.java
+++ src/tema2ManejoConectores/ejercicios10/NavalvintedAPP.java
@@ -47,7 +47,7 @@ public class NavalvintedAPP {
 			procutosPorTallas();
 			break;
 		case 3:
-
+			crearProducto();
 			break;
 		case 4:
 
@@ -65,39 +65,194 @@ public class NavalvintedAPP {
 
 	}
 
-	private static void procutosPorTallas() {
-		String talla = "";
-		System.out.println("Introduce la talla");
-		talla = entrada.nextLine().toUpperCase();
-		
-		String consulta = "SELECT nombre_Producto, estado, precio, t.talla "
-                + "FROM producto p "
-                + "JOIN talla t ON p.id_Talla = t.id_Talla "
-                + "WHERE t.talla = ?;";
+	private static void crearProducto() {
 
+		String nombreProducto, estado;
+		int categoria = 0, talla = 0, id_color = 0, id_Material=0, stock=0, descuento=0;
+		double precio, costo;
+		
+
+		System.out.println("Introduce el nombre del producto");
+		nombreProducto = entrada.nextLine();
+
+		categoria = pedirCategoria();
+
+		talla = elegir_Talla();
+
+		id_color = pedir_Color();
+
+		id_Material = pedir_Material();
+		
+		System.out.println("Inserte stock:");
+		stock= Integer.parseInt(entrada.nextLine());
+		
+		
+		System.out.println("Inserte el precio del producto");
+		precio= Double.parseDouble(entrada.nextLine());
+		
+		System.out.println("Costo del producto");
+		costo= Double.parseDouble(entrada.nextLine());
+	
+		System.out.println("Estado del producto:");
+		estado= entrada.nextLine();
+		
+		System.out.println("Escribe el descuento que tiene");
+		descuento = Integer.parseInt(entrada.nextLine());
+		
+		String insercion = "INSERT INTO producto (nombre_Producto, id_categoria, id_Talla, id_Color,"
+				+ "id_Material, stock, precio, costo, estado, descuento)VALUES(?,?,?,?,?,?,?,?,?,?);";
 		
 		try {
-			PreparedStatement ps = con.prepareStatement(consulta);
+			PreparedStatement ps = con.prepareStatement(insercion);
+			ps.setString(1, nombreProducto);
+			ps.setInt(2, categoria);
+			ps.setInt(3, talla);
+			ps.setInt(4, id_color);
+			ps.setInt(5, id_Material);
+			ps.setInt(6, stock);
+			ps.setDouble(7, precio);
+			ps.setDouble(8, costo);
+			ps.setString(9, estado);
+			ps.setInt(10, descuento);
 			
-			ps.setString(1, talla);
-
-			ResultSet rs = ps.executeQuery();
+		
 			
-			while (rs.next()) {
-				System.out.println("Talla: "+rs.getString("talla"));
-				System.out.println("Nombre: "+rs.getString("nombre_Producto"));
-				System.out.println("Estado "+rs.getString("estado"));
-				System.out.println("Precio "+rs.getDouble("precio"));
+			// Ejecutamos la inserción
+			int filasInsertadas = ps.executeUpdate();
+			if (filasInsertadas > 0) {
+			System.out.println("Insertado "+
+					ps.getUpdateCount()+" registros");
 			}
 			
+			
+			
+			
+			ps.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+	
+	}
+
+	private static int pedir_Material() {
+		int id_Material = 0;
+		do {
+			System.out.println("1. Algodon");
+			System.out.println("2. Mezclilla");
+			System.out.println("3. Poliester");
+			System.out.println("4. Lino");
+			System.out.println("5. Sintetico");
+			System.out.println("6. Cuero");
 		
+			id_Material = Integer.parseInt(entrada.nextLine());
+			if (id_Material < 1 || id_Material > 6) {
+				System.out.println("Opcion no válida, repita la operacion");
+			}
+
+		} while (id_Material < 1 || id_Material > 6);
 		
+		return id_Material;
+	}
+
+	private static int pedir_Color() {
+		int id_color = 0;
+		do {
+			System.out.println("1. Azul");
+			System.out.println("2. Negro");
+			System.out.println("3. Rojo");
+			System.out.println("4. Gris");
+			System.out.println("5. Verde");
+			System.out.println("6. Blanco");
+			System.out.println("7. Beige");
+			System.out.println("8. Marron");
+			id_color = Integer.parseInt(entrada.nextLine());
+			if (id_color < 1 || id_color > 8) {
+				System.out.println("Opcion no válida, repita la operacion");
+			}
+
+		} while (id_color < 1 || id_color > 8);
 		
+		return id_color;
+	}
+
+	private static int elegir_Talla() {
+
+		int talla = 0;
+		do {
+			System.out.println("Elije la talla");
+			System.out.println("1. S");
+			System.out.println("2. M");
+			System.out.println("3. L");
+			System.out.println("4. XL");
+			System.out.println("5. 42");
+			System.out.println("6. 39");
+			talla = Integer.parseInt(entrada.nextLine());
+			if (talla < 1 || talla > 6) {
+				System.out.println("Opcion no válida, repita la operacion");
+			}
+
+		} while (talla < 1 || talla > 6);
+
+		return talla;
+	}
+
+	private static int pedirCategoria() {
+		int categoria;
+
+		do {
+			System.out.println("Elija la categoria:");
+			System.out.println("1. Camisas:");
+			System.out.println("2. Pantalones");
+			System.out.println("3. Vestidos");
+			System.out.println("4. Abrigos");
+			System.out.println("5. Faldas");
+			System.out.println("6. Calzado");
+			categoria = Integer.parseInt(entrada.nextLine());
+
+			if (categoria < 1 || categoria > 6) {
+				System.out.println("Opcion no válida, repita la operacion");
+			}
+
+		} while (categoria < 1 || categoria > 6);
+
+		return categoria;
+	}
+
+	private static void procutosPorTallas() {
+		String talla = "";
+		System.out.println("Introduce la talla");
+		talla = entrada.nextLine().toUpperCase();
+
+		String consulta = "SELECT nombre_Producto, estado, precio, t.talla " + "FROM producto p "
+				+ "JOIN talla t ON p.id_Talla = t.id_Talla " + "WHERE t.talla = ?;";
+
+		try {
+			PreparedStatement ps = con.prepareStatement(consulta);
+
+			ps.setString(1, talla);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				System.out.println("Talla: " + rs.getString("talla"));
+				System.out.println("Nombre: " + rs.getString("nombre_Producto"));
+				System.out.println("Estado " + rs.getString("estado"));
+				System.out.println("Precio " + rs.getDouble("precio"));
+			}
+
+			if (!rs.next()) {
+				System.out.println("No existe la talla");
+			}
+
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	private static void productoPorcategorias() {
@@ -123,10 +278,13 @@ public class NavalvintedAPP {
 				System.out.println("Precio: " + rs.getString("precio"));
 				System.out.println("-----------------------");
 			}
-			
+
+			if (!rs.next()) {
+				System.out.println("No existe la talla");
+			}
+
 			rs.close();
 			ps.close();
-			
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
