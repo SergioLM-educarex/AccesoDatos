e49d49e385e9eae625d91f6e727fda76062d6f5a
SergioLM-educarex <sleonm03@educarex.es>
2026-02-05 09:09:16 +0100

    ser

---
+++ src/tema4mapeobjetorelacional/ejerciciosJPAalumnos/operacionessencillascontabla/Ejercicio6.java
@@ -0,0 +1,10 @@
+package tema4mapeobjetorelacional.ejerciciosJPAalumnos.operacionessencillascontabla;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
